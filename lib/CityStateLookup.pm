8ff02ec1808395e45a3039073b2a2db47f1b851d
brian d foy <brian.d.foy@gmail.com>
2015-01-03 17:04:35 -0500

    Update copyright statement

--- lib/CityStateLookup.pm
+++ lib/CityStateLookup.pm
@@ -149,7 +149,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2006-2007, brian d foy, All Rights Reserved.
+Copyright © 2006-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
--- lib/ZipCodeLookup.pm
+++ lib/ZipCodeLookup.pm
@@ -155,7 +155,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2006-2007 brian d foy.  All rights reserved.
+Copyright © 2006-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
