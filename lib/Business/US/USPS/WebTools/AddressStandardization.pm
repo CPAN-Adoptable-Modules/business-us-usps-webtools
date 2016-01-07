6029a4708b204ff086e7b51765718913bcca18cd
brian d foy <brian.d.foy@gmail.com>
2016-01-06 23:20:02 -0500

    Update copyright

--- lib/Business/US/USPS/WebTools/AddressStandardization.pm
+++ lib/Business/US/USPS/WebTools/AddressStandardization.pm
@@ -169,7 +169,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2006-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2006-2016, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
